5134e466c6bcb4336f53410967655178d42a26c0
Mike Wong <wongmdesign@gmail.com>
2020-02-23 11:13:30 -0500

    implemented login state and action to utilize redux store and reducer in app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "./App.scss";
 
@@ -8,31 +9,27 @@ import ShopPage from "./components/Pages/Shop/Shop";
 import Header from "./components/Header/Header";
 import SignInSignUp from "./components/Pages/SignInSignUp/SignInSignUp";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/actions";
 
-export default class App extends Component {
-  state = {
-    currentUser: null
-  };
-
+class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     // open subscription/messaging between app and firebase app
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       } else {
         // if userAuth doesn't exist, then set it to the current state which is null
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -55,3 +52,9 @@ export default class App extends Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
